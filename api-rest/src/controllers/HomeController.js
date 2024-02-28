a3fcb616d2d03cb826d493efd21f4ff4055d0561
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-02-28 14:06:27 -0300

    create aluno routes

--- api-rest/src/controllers/HomeController.js
+++ api-rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Pedro',
-      sobrenome: 'neto',
-      email: 'pn54781@gmail.com',
-      idade: 19,
-      peso: 80,
-      altura: 1.80,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
