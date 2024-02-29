d9fdd47e3231b75dd8d05fbc3b19ba71ca3d5d4c
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-02-29 12:53:22 -0300

    close fotoroutes with loginRequired

--- api-rest/src/routes/fotoRoutes.js
+++ api-rest/src/routes/fotoRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
 import fotoRoutes from '../controllers/FotoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/', fotoRoutes.store);
+router.post('/', loginRequired, fotoRoutes.store);
 
 export default router;
